c5abdbfc92237701c55c4e00cd098b874a151c84
Keiichiro Ono <keiono@gmail.com>
2019-04-25 16:06:22 -0700

    React wrapper fully updated to the functional style

--- packages/CyTreeViewer/src/CirclePackingRenderer/index.jsx
+++ packages/CyTreeViewer/src/CirclePackingRenderer/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import CirclePacking, {
   selectNodes,
   highlightNode,
@@ -14,14 +14,11 @@ const CirclePackingRenderer = props => {
 
   // For initialization
   useEffect(() => {
-    console.log('trying to mount:', props, treeRef)
     CirclePacking(props.tree, treeRef.current, props.width, props.height, props)
-    console.log('hooks! called---------')
+    console.log('CP initialized!')
   }, [])
 
   useEffect(() => {
-    console.log('hooks! COMMAND---------')
-
     const command = props.command
     if (command.command === 'fit') {
       fit()
@@ -31,62 +28,20 @@ const CirclePackingRenderer = props => {
   }, [props.command])
 
   useEffect(() => {
-
-    console.log('hooks! Selection---------')
     selectNodes(props.selected)
   }, [props.selected])
 
   useEffect(() => {
-
-    console.log('hooks! Highlight---------')
     highlightNode(props.highlight)
   }, [props.highlight])
 
-  // componentWillReceiveProps(nextProps) {
-  //
-  //   console.log('Will CP rendering!!!:', nextProps)
-  //
-  //   const rootColor = this.props.rendererOptions.rootColor
-  //   const leafColor = this.props.rendererOptions.leafColor
-  //   const newRootColor = nextProps.rendererOptions.rootColor
-  //   const newLeafColor = nextProps.rendererOptions.leafColor
-  //
-  //   const newHeight = nextProps.height
-  //
-  //   if (
-  //     this.props.height !== newHeight ||
-  //     rootColor !== newRootColor ||
-  //     leafColor !== newLeafColor
-  //   ) {
-  //     console.log('CP rendering!!!:', nextProps)
-  //     CirclePacking(
-  //       nextProps.tree,
-  //       this.tree,
-  //       nextProps.width,
-  //       nextProps.height,
-  //       nextProps
-  //     )
-  //   }
-  //
-  //   const command = nextProps.command
-  //   if(command !== null && command !== this.props.command) {
-  //     if(command.command === 'fit') {
-  //       fit()
-  //     } else if(command.command === 'reset') {
-  //       clear()
-  //     }
-  //   }
-  //
-  //   if (nextProps.selected !== this.props.selected) {
-  //     console.log('SELECT called:', nextProps)
-  //     selectNodes(nextProps.selected)
-  //   }
-  //
-  //   if(!nextProps.highlight && nextProps.highlight !== this.props.highlight) {
-  //     console.log('HIGHLIGHT: ', this.props.highlight)
-  //     highlightNode(this.props.highlight)
-  //   }
-  // }
+  useEffect(() => {
+    CirclePacking(props.tree, treeRef.current, props.width, props.height, props)
+  }, [
+    props.rendererOptions.rootColor,
+    props.rendererOptions.leafColor,
+    props.height
+  ])
 
   return <div ref={treeRef} />
 }
